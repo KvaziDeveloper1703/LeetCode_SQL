56ad0294638d1417e38d50582418854e51dbea29
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 02:52:02 +0300

    Modified 584

--- 584_Find_Customer_Referee.sql
+++ 584_Find_Customer_Referee.sql
@@ -1,7 +1,7 @@
 /*
-We have a table Customer with the following columns:
-+ id (integer, primary key).
-+ name (string, customer's name).
+We have a table "Customer" with the following columns:
++ id (integer, primary key);
++ name (string, customer's name);
 + referee_id (integer, ID of the customer who referred them).
 
 Each row in the table represents a customer, their name, and referee_id, which indicates who referred them (if any). If referee_id is NULL, it means the customer was not referred by anyone.
@@ -10,9 +10,9 @@ Write an SQL query to find the names of all customers who were NOT referred by t
 
 Return a table with a single column name, containing the names of such customers. The result order can be arbitrary.
 
-У нас есть таблица Customer со следующими колонками:
-+ id (целое число, первичный ключ).
-+ name (строка, имя клиента).
+У нас есть таблица "Customer" со следующими колонками:
++ id (целое число, первичный ключ);
++ name (строка, имя клиента);
 + referee_id (целое число, id клиента, который порекомендовал данного клиента).
 
 Каждая строка таблицы представляет клиента, его имя и referee_id, указывающий, кто его порекомендовал (если есть). Если referee_id имеет значение NULL, значит, этот клиент никем не был рекомендован.
