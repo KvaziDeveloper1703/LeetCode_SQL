7658dbe676000b87f62c5bf098d6370c9d20dc91
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 02:18:23 +0300

    Modified task 570

--- 570_Managers_with_at_Least_5_Direct_Reports.sql
+++ 570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -1,35 +1,34 @@
 /*
-You are given a table named Employee, which contains information about employees in a company.
+You are given a table named "Employee", which contains information about employees in a company.
 
 Table Description:
-+ Each row in the table represents an employee, including their ID, name, department, and the ID of their manager.
-+ id is the primary key (a unique identifier for each employee).
-+ name is the employee’s name.
-+ department represents the department in which the employee works.
-+ managerId is the ID of the employee’s manager. If managerId is NULL, the employee does not have a manager.
++ Each row in the table represents an employee, including their ID, name, department, and the ID of their manager;
++ id is the primary key;
++ name is the employee’s name;
++ department represents the department in which the employee works;
++ managerId is the ID of the employee’s manager. If managerId is NULL, the employee does not have a manager;
 + No employee is their own manager.
 
-Write a query to find all managers who have at least five direct reports (employees directly managed by them).
+Write a query to find all managers who have at least five direct reports.
 
 Return the result in any order.
 
 Дана таблица Employee, содержащая информацию о сотрудниках компании.
 
 Описание таблицы:
-+ Каждая строка в таблице представляет одного сотрудника, включая его ID, имя, отдел и ID его начальника.
-+ id — первичный ключ (уникальный идентификатор каждого сотрудника).
-+ name — имя сотрудника.
-+ department — отдел, в котором работает сотрудник.
-+ managerId — ID начальника сотрудника. Если значение NULL, значит у сотрудника нет руководителя.
++ Каждая строка в таблице представляет одного сотрудника, включая его ID, имя, отдел и ID его начальника;
++ id — первичный ключ;
++ name — имя сотрудника;
++ department — отдел, в котором работает сотрудник;
++ managerId — ID начальника сотрудника. Если значение NULL, значит у сотрудника нет руководителя;
 + Никто не может быть сам себе начальником.
 
-Напишите SQL-запрос, который найдёт всех менеджеров, у которых есть как минимум 5 подчинённых (сотрудников, которых они непосредственно управляют).
+Напишите SQL-запрос, который найдёт всех менеджеров, у которых есть как минимум 5 подчинённых.
 
 Верните результат в любом порядке.
 */
 
 SELECT Employee.name
-FROM Employee
-JOIN Employee AS Subordinate ON Employee.id = Subordinate.managerId
+FROM Employee JOIN Employee AS Subordinate ON Employee.id = Subordinate.managerId
 GROUP BY Employee.id, Employee.name
 HAVING COUNT(Subordinate.id) >= 5;
