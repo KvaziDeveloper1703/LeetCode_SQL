42bb64d252507e9df5adacd38562a6d10ceaccba
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 03:32:05 +0300

    Modified 1661

--- 1661_Average_Time_of_Process_per_Machine.sql
+++ 1661_Average_Time_of_Process_per_Machine.sql
@@ -1,13 +1,13 @@
 /*
-You are given a table named Activity with the following structure:
-+ (machine_id, process_id, activity_type) is the primary key, meaning this combination of columns contains unique values.
-+ machine_id represents the ID of a machine.
-+ process_id represents the ID of a process running on a machine.
-+ activity_type is an ENUM with two possible values: 'start' and 'end'.
-+ timestamp is a float representing the time in seconds.
-+ 'start' means that the machine started the process at the given timestamp.
-+ 'end' means that the machine ended the process at the given timestamp.
-+ It is guaranteed that each (machine_id, process_id) pair has both a 'start' and 'end' timestamp.
+You are given a table named "Activity" with the following structure:
++ (machine_id, process_id, activity_type) is the primary key, meaning this combination of columns contains unique values;
++ machine_id represents the ID of a machine;
++ process_id represents the ID of a process running on a machine;
++ activity_type is an ENUM with two possible values: 'start' and 'end';
++ timestamp is a float representing the time in seconds;
++ 'start' means that the machine started the process at the given timestamp;
++ 'end' means that the machine ended the process at the given timestamp;
++ It is guaranteed that each (machine_id, process_id) pair has both a 'start' and 'end' timestamp;
 + The 'start' timestamp will always be before the 'end' timestamp.
 
 A factory website manages multiple machines, each running the same number of processes. Write an SQL query to calculate the average time each machine takes to complete a process.
@@ -15,30 +15,30 @@ The time to complete a process is calculated as: end_timestamp - start_timestamp
 
 The average processing time for each machine is computed by summing up all process times and dividing by the number of processes.
 The result table should contain:
-+ machine_id
-+ processing_time (rounded to three decimal places)
++ machine_id;
++ processing_time;
 
 Return the result table in any order.
 
 Вам дана таблица Activity со следующей структурой:
-+ (machine_id, process_id, activity_type) — это первичный ключ, т.е. комбинация этих колонок содержит уникальные значения.
-+ machine_id — идентификатор машины.
-+ process_id — идентификатор процесса, выполняемого на машине.
-+ activity_type — это ENUM с возможными значениями 'start' и 'end'.
-+ timestamp — это число с плавающей точкой, обозначающее время в секундах.
-+ 'start' означает, что машина запустила процесс в указанное время.
-+ 'end' означает, что машина завершила процесс в указанное время.
-+ Гарантируется, что у каждой пары (machine_id, process_id) есть как 'start', так и 'end'.
++ (machine_id, process_id, activity_type) — это первичный ключ, т.е. комбинация этих колонок содержит уникальные значения;
++ machine_id — идентификатор машины;
++ process_id — идентификатор процесса, выполняемого на машине;
++ activity_type — это ENUM с возможными значениями 'start' и 'end';
++ timestamp — это число с плавающей точкой, обозначающее время в секундах;
++ 'start' означает, что машина запустила процесс в указанное время;
++ 'end' означает, что машина завершила процесс в указанное время;
++ Гарантируется, что у каждой пары (machine_id, process_id) есть как 'start', так и 'end';
 + Время 'start' всегда меньше времени 'end'.
 
 На заводском сайте работают несколько машин, каждая из которых выполняет одинаковое количество процессов. Напишите SQL-запрос, который вычислит среднее время выполнения процесса для каждой машины.
-Время выполнения процесса вычисляется как: end_timestamp - 
+Время выполнения процесса вычисляется как: end_timestamp - start_timestamp
 
 Среднее время выполнения для каждой машины рассчитывается как сумма всех временных интервалов, деленная на количество процессов.
 
 Результирующая таблица должна содержать:
-+ machine_id
-+ processing_time (округленное до трёх знаков после запятой)
++ machine_id;
++ processing_time;
 
 Верните таблицу в любом порядке.
 */
