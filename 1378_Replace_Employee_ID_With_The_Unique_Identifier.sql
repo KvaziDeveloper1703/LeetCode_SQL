0d8e3cd9af6ae65bb80d4c75b490025707386acd
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 03:21:52 +0300

    Modified 1378

--- 1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
+++ 1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -1,12 +1,12 @@
 /*
 We have two tables:
 
-+ Table Employees:
++ Table "Employees":
     + id (integer, unique employee identifier, primary key);
     + name (string, employee name).
 This table contains a list of all employees in the company.
 
-+ Table EmployeeUNI:
++ Table "EmployeeUNI":
     + id (integer, employee ID);
     + unique_id (integer, employee's unique ID).
 The primary key for this table is the combination of (id, unique_id).
@@ -21,6 +21,29 @@ Return a table with the following columns:
 + name (employee's name).
 
 The result order can be arbitrary.
+
+У нас есть две таблицы:
+
++ Таблица "Employees":
+    + id (целое число, уникальный идентификатор сотрудника, первичный ключ);
+    + name (строка, имя сотрудника).
+Эта таблица содержит список всех сотрудников компании.
+
++ Таблица "EmployeeUNI":
+    + id (целое число, идентификатор сотрудника);
+    + unique_id (целое число, уникальный идентификатор сотрудника).
+Первичным ключом этой таблицы является комбинация (id, unique_id).
+Эта таблица сопоставляет обычные идентификаторы сотрудников с их уникальными идентификаторами.
+Однако не у всех сотрудников есть уникальный идентификатор, поэтому некоторые id из таблицы Employees могут отсутствовать в таблице EmployeeUNI.
+
+Напишите SQL-запрос, который объединит данные из обеих таблиц и вернёт уникальный идентификатор (unique_id) каждого сотрудника.
+Если у сотрудника нет unique_id, в соответствующей колонке должно быть значение NULL.
+
+Верните таблицу со следующими колонками:
++ unique_id (уникальный идентификатор сотрудника или NULL, если его нет);
++ name (имя сотрудника).
+
+Порядок строк в результате может быть произвольным.
 */
 
 SELECT EmployeeUNI.unique_id, Employees.name
