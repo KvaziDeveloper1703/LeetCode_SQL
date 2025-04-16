73318b1abf301e02d2e6711e5a5f0f1a35248d78
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-16 19:36:54 +0300

    Corrected task 1251

--- 1251_Average_Selling_Price.sql
+++ 1251_Average_Selling_Price.sql
@@ -30,10 +30,11 @@ Find the average selling price for each product, rounded to 2 decimal places. If
 
 SELECT  Prices.product_id,
         ROUND(
-            COALESCE(SUM(UnitsSold.units * Prices.price), 0.0) / NULLIF(SUM(UnitsSold.units), 0),
+            COALESCE(SUM(UnitsSold.units * Prices.price), 0.0) /
+            COALESCE(SUM(UnitsSold.units), 1),
             2
         ) AS average_price
 FROM Prices
-LEFT JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id 
+LEFT JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id
                     AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
 GROUP BY Prices.product_id;
