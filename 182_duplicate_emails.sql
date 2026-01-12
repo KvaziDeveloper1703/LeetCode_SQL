3f894c5aa96bba18630bbd7d6f0bc2addd19c2f7
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-12 07:58:08 +0200

    Added a finnish translation to task 182

--- 182_duplicate_emails.sql
+++ 182_duplicate_emails.sql
@@ -1,19 +1,27 @@
 /*
 You are given a table called Person with the following columns:
-+ id: Primary key (unique for each person)
-+ email: Email address (all lowercase, never NULL)
+    - id
+    - email
 
-Write a SQL query to return all duplicate email addresses — i.e., emails that appear more than once in the table.
+Write a SQL query to return all duplicate email addresses - i.e., emails that appear more than once in the table.
 
 The result should have one column: email. The output can be in any order.
 
 Дана таблица Person со следующими столбцами:
-+ id: идентификатор (уникальный для каждого человека)
-+ email: адрес электронной почты (в нижнем регистре, не бывает NULL)
+    - id
+    - email
 
 Напишите SQL-запрос, который вернёт все повторяющиеся email-адреса, то есть такие, которые встречаются больше одного раза.
 
-Результирующая таблица должна содержать один столбец: email. Порядок строк в результате — любой.
+Результирующая таблица должна содержать один столбец: email. Порядок строк в результате - любой.
+
+Sinulle on annettu taulu Person, jossa on seuraavat sarakkeet:
+    - id
+    - email
+
+Kirjoita SQL-kysely, joka palauttaa kaikki toistuvat sähköpostiosoitteet, eli ne, jotka esiintyvät useammin kuin kerran.
+
+Tulostaulussa tulee olla yksi sarake: email. Rivien järjestyksellä ei ole merkitystä.
 */
 
 SELECT email
