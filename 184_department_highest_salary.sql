ef7cfeb937b5d6a6555c8a9d5f5be8932fa50592
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-12 08:10:30 +0200

    Added a finnish translation to task 184

--- 184_department_highest_salary.sql
+++ 184_department_highest_salary.sql
@@ -1,51 +1,77 @@
 /*
 You are given two tables:
 
-Employee
-+ id: Unique employee ID (primary key)
-+ name: Name of the employee
-+ salary: Salary of the employee
-+ departmentId: Foreign key referencing Department.id
+Employee:
+    - id
+    - name
+    - salary
+    - departmentId
 
-Department
-+ id: Unique department ID (primary key)
-+ name: Name of the department (never NULL)
+Department:
+    - id
+    - name
 
 Write a SQL query to find the employees with the highest salary in each department.
 
 Return a table with the following columns:
-+ Department — department name
-+ Employee — employee name
-+ Salary — employee salary
+    - Department - department name;
+    - Employee - employee name;
+    - Salary - employee salary.
 
 The output can be in any order.
 
 Даны две таблицы:
 
-Employee
-+ id: уникальный идентификатор сотрудника (первичный ключ)
-+ name: имя сотрудника
-+ salary: зарплата
-+ departmentId: внешний ключ на Department.id
+Employee:
+    - id
+    - name
+    - salary
+    - departmentId
 
 Department
-+ id: уникальный идентификатор отдела (первичный ключ)
-+ name: название отдела (не бывает NULL)
+    - id
+    - name
 
 Напишите SQL-запрос, который вернёт сотрудников с наибольшей зарплатой в каждом отделе.
 
 Результат должен содержать следующие столбцы:
-+ Department — название отдела
-+ Employee — имя сотрудника
-+ Salary — его зарплата
+    - Department - название отдела;
+    - Employee - имя сотрудника;
+    - Salary - его зарплата.
 
-Порядок строк — любой.
+Порядок строк любой.
+
+Sinulle on annettu kaksi taulua:
+
+Employee:
+    - id
+    - name
+    - salary
+    - departmentId
+
+Department:
+    - id
+    - name
+
+Kirjoita SQL-kysely, joka palauttaa kunkin osaston korkeimman palkan saavat työntekijät.
+
+Tuloksen tulee sisältää seuraavat sarakkeet:
+    - Department - osaston nimi;
+    - Employee - työntekijän nimi;
+    - Salary - hänen palkkansa.
+
+Rivien järjestyksellä ei ole merkitystä.
 */
 
-SELECT Department.name AS Department, Employee.name AS Employee, Employee.salary AS Salary
+SELECT
+    Department.name AS Department,
+    Employee.name   AS Employee,
+    Employee.salary AS Salary
 FROM Employee
-JOIN Department ON Employee.departmentId = Department.id
-WHERE Employee.salary = (   SELECT MAX(EmployeeSub.salary)
-                            FROM Employee AS EmployeeSub
-                            WHERE EmployeeSub.departmentId = Employee.departmentId
-                    );
+JOIN Department
+    ON Employee.departmentId = Department.id
+WHERE Employee.salary = (
+    SELECT MAX(EmployeeSub.salary)
+    FROM Employee AS EmployeeSub
+    WHERE EmployeeSub.departmentId = Employee.departmentId
+);
