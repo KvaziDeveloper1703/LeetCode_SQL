e15d13802479cd8ce5afbc3c57ad10481c2b0589
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-12 04:52:21 +0200

    Minor corrections to task 176

--- 176_Second_Highest_Salary.sql
+++ 176_Second_Highest_Salary.sql
@@ -1,33 +1,32 @@
 /*
 You are given a table:
-+ Employee:
-    + id: unique ID of the employee (primary key);
-    + salary: salary of the employee.
+
+Employee:
+    - id
+    - salary
 
 Each row represents the salary of one employee.
-
 Write an SQL query to return the second highest distinct salary from the Employee table.
 If there is no second highest salary, return null.
 
-Return the result with the following column:
-+ SecondHighestSalary
+Return the result with the following column: SecondHighestSalary.
 
 Дана таблица:
-+ Employee:
-    + id: уникальный идентификатор сотрудника (первичный ключ);
-    + salary: зарплата сотрудника.
+
+Employee:
+    - id
+    - salary
 
 Каждая строка содержит информацию о зарплате одного сотрудника.
-
 Напиши SQL-запрос, который вернёт вторую по величине уникальную зарплату из таблицы.
 Если второй зарплаты не существует, верни null.
 
-Результат должен содержать одну колонку:
-+ SecondHighestSalary
+Результат должен содержать одну колонку: SecondHighestSalary.
 */
 
-SELECT (SELECT DISTINCT salary
+SELECT (   
+        SELECT DISTINCT salary
         FROM Employee
         ORDER BY salary DESC
         LIMIT 1 OFFSET 1
-    ) AS SecondHighestSalary;
+) AS SecondHighestSalary;
