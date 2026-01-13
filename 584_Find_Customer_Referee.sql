9d1f0d60ce0b0e2a7ee89bd7535e3b70672aadb4
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-13 07:52:56 +0200

    Added a finnish translation to task 584

--- 584_Find_Customer_Referee.sql
+++ 584_Find_Customer_Referee.sql
@@ -1,21 +1,21 @@
 /*
-We have a table "Customer" with the following columns:
-+ id (integer, primary key);
-+ name (string, customer's name);
-+ referee_id (integer, ID of the customer who referred them).
+We have a table Customer:
+    - id
+    - name
+    - referee_id
 
-Each row in the table represents a customer, their name, and referee_id, which indicates who referred them (if any). If referee_id is NULL, it means the customer was not referred by anyone.
+Each row in the table represents a customer, their name, and referee_id, which indicates who referred them. If referee_id is NULL, it means the customer was not referred by anyone.
 
 Write an SQL query to find the names of all customers who were NOT referred by the customer with id = 2.
 
 Return a table with a single column name, containing the names of such customers. The result order can be arbitrary.
 
-У нас есть таблица "Customer" со следующими колонками:
-+ id (целое число, первичный ключ);
-+ name (строка, имя клиента);
-+ referee_id (целое число, id клиента, который порекомендовал данного клиента).
+У нас есть таблица Customer:
+    - id
+    - name
+    - referee_id
 
-Каждая строка таблицы представляет клиента, его имя и referee_id, указывающий, кто его порекомендовал (если есть). Если referee_id имеет значение NULL, значит, этот клиент никем не был рекомендован.
+Каждая строка таблицы представляет клиента, его имя и referee_id, указывающий, кто его порекомендовал. Если referee_id имеет значение NULL, значит, этот клиент никем не был рекомендован.
 
 Написать SQL-запрос, который выбирает имена всех клиентов, которых НЕ порекомендовал клиент с id = 2.
 
