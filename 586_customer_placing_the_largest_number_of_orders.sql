1aa090c1d025ae01cd419a194922c011f4730e94
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-13 08:01:07 +0200

    Added a finnish translation to task 586

--- 586_customer_placing_the_largest_number_of_orders.sql
+++ 586_customer_placing_the_largest_number_of_orders.sql
@@ -1,15 +1,18 @@
 /*
 You are given a table Orders with information about orders and customers.
 Each row represents one order made by a customer. order_number is unique.
-
 Find the customer_number of the customer who has placed the largest number of orders.
 It is guaranteed that exactly one customer has more orders than all others.
 
 Дана таблица Orders с информацией о заказах и клиентах.
-Каждая строка соответствует одному заказу клиента. order_number — уникальный.
-
+Каждая строка соответствует одному заказу клиента. order_number уникален.
 Найдите customer_number клиента, который сделал наибольшее количество заказов.
 Гарантируется, что такой клиент единственный.
+
+Annetaan taulu Orders, joka sisältää tietoa tilauksista ja asiakkaista.
+Jokainen rivi vastaa yhtä asiakkaan tekemää tilausta. order_number on uniikki.
+Etsi sen asiakkaan customer_number, joka on tehnyt eniten tilauksia.
+Taataan, että tällainen asiakas on ainoa.
 */
 
 SELECT customer_number
