6cbf7171e5aceba583d06b9c1169f9bf68e08b0e
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-13 07:41:27 +0200

    Added a finnish translation to task 570

--- 570_Managers_with_at_Least_5_Direct_Reports.sql
+++ 570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -1,31 +1,36 @@
 /*
-You are given a table named "Employee", which contains information about employees in a company.
+You are given a table Employee:
+    - id
+    - name
+    - department
+    - managerId
 
-Table Description:
-+ Each row in the table represents an employee, including their ID, name, department, and the ID of their manager;
-+ id is the primary key;
-+ name is the employee’s name;
-+ department represents the department in which the employee works;
-+ managerId is the ID of the employee’s manager. If managerId is NULL, the employee does not have a manager;
-+ No employee is their own manager.
+No employee is their own manager.
 
 Write a query to find all managers who have at least five direct reports.
-
 Return the result in any order.
 
-Дана таблица Employee, содержащая информацию о сотрудниках компании.
+Дана таблица Employee:
+    - id
+    - name
+    - department
+    - managerId
 
-Описание таблицы:
-+ Каждая строка в таблице представляет одного сотрудника, включая его ID, имя, отдел и ID его начальника;
-+ id — первичный ключ;
-+ name — имя сотрудника;
-+ department — отдел, в котором работает сотрудник;
-+ managerId — ID начальника сотрудника. Если значение NULL, значит у сотрудника нет руководителя;
-+ Никто не может быть сам себе начальником.
+Никто не может быть сам себе начальником.
 
 Напишите SQL-запрос, который найдёт всех менеджеров, у которых есть как минимум 5 подчинённых.
-
 Верните результат в любом порядке.
+
+Annetaan taulu Employee:
+    - id
+    - name
+    - department
+    - managerId
+
+Kukaan ei voi olla itsensä esihenkilö.
+
+Kirjoita SQL-kysely, joka löytää kaikki esihenkilöt, joilla on vähintään 5 alaista.
+Palauta tulos missä tahansa järjestyksessä.
 */
 
 SELECT Employee.name
