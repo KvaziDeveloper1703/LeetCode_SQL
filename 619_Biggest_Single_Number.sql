679b555d2cbc48a0cad224d112171bd438eefdea
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-14 06:01:44 +0200

    Added a finnish translation to task 619

--- 619_Biggest_Single_Number.sql
+++ 619_Biggest_Single_Number.sql
@@ -1,24 +1,36 @@
 /*
-You are given a table:
-+ MyNumbers:
-    + num: An integer value;
-    + The table may contain duplicates (no primary key).
+You are given a table MyNumbers:
+    - num
+
+The table may contain duplicates.
 
 A single number is one that appears only once in the table.
-Write an SQL query to find the largest single number. If there are no single numbers, return null.
+Write an SQL query to find the largest single number. 
+If there are no single numbers, return null.
 
-Дана таблица:
-+ MyNumbers:
-    + num: целое число;
-    + В таблице могут быть дубликаты (первичного ключа нет).
+Дана таблица MyNumbers:
+    - num
 
-Одиночное число — это число, которое встречается только один раз в таблице.
-Напиши SQL-запрос, который вернёт наибольшее одиночное число. Если таких чисел нет — верни null.
+В таблице могут быть дубликаты.
+
+Одиночное число - это число, которое встречается только один раз в таблице.
+Напишите SQL-запрос, который вернёт наибольшее одиночное число.
+Если таких чисел нет - верните null.
+
+Annettu taulukko MyNumbers:
+    - num
+
+Taulukossa voi olla duplikaatteja.
+
+Yksittäinen luku on luku, joka esiintyy taulukossa vain kerran.
+Kirjoita SQL-kysely, joka palauttaa suurimman yksittäisen luvun.
+Jos tällaisia lukuja ei ole, palauta null.
 */
 
 SELECT MAX(num) AS num
-FROM (  SELECT num
-        FROM MyNumbers
-        GROUP BY num
-        HAVING COUNT(*) = 1
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
 ) AS SingleNumbers;
