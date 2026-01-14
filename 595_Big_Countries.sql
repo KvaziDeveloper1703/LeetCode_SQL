da03ffd68a50b9906a16905dd248a43c3ece8996
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-14 05:34:15 +0200

    Added a finnish translation to task 595

--- 595_Big_Countries.sql
+++ 595_Big_Countries.sql
@@ -1,35 +1,48 @@
 /*
-We have a table "World" with the following columns:
-+ name (string, unique country name, primary key);
-+ continent (string, continent the country belongs to);
-+ area (integer, country's area in square kilometers);
-+ population (integer, country's population);
-+ gdp (bigint, country's gross domestic product).
+We have a table World:
+    - name
+    - continent
+    - area
+    - population
+    - gdp
 
-Definition of a "big country"
 A country is considered big if at least one of the following conditions is met:
-+ Its area is at least 3,000,000 km² (area >= 3000000), or
-+ Its population is at least 25,000,000 (population >= 25000000).
+    - Its area is at least 3,000,000 km², or
+    - Its population is at least 25,000,000.
 
 Write an SQL query to find the name, population, and area of all big countries.
+Return a table with the columns name, population, and area, containing information only about big countries.
+The result order can be arbitrary.
 
-Return a table with the columns name, population, and area, containing information only about big countries. The result order can be arbitrary.
+У нас есть таблица World:
+    - name
+    - continent
+    - area
+    - population
+    - gdp
 
-У нас есть таблица "World" со следующими колонками:
-+ name (строка, уникальное имя страны, первичный ключ);
-+ continent (строка, континент, на котором расположена страна);
-+ area (целое число, площадь страны в квадратных километрах);
-+ population (целое число, население страны);
-+ gdp (bigint, валовой внутренний продукт страны).
-
-Определение "большой страны"
 Страна считается большой, если выполняется хотя бы одно из условий:
-+ её площадь не менее 3 000 000 км² (area >= 3000000), или
-+ её население не менее 25 000 000 человек (population >= 25000000).
+    - её площадь не менее 3 000 000 км², или
+    - её население не менее 25 000 000 человек.
 
 Написать SQL-запрос, который находит имена, население и площадь всех больших стран.
+Вывести таблицу с колонками name, population, area, содержащую информацию только о больших странах.
+Порядок строк в ответе может быть произвольным.
 
-Вывести таблицу с колонками name, population, area, содержащую информацию только о больших странах. Порядок строк в ответе может быть произвольным.
+Meillä on taulukko World:
+    - name
+    - continent
+    - area
+    - population
+    - gdp
+
+Maa katsotaan suureksi, jos vähintään yksi seuraavista ehdoista täyttyy:
+    - sen pinta-ala on vähintään 3 000 000 km², tai
+    - sen väkiluku on vähintään 25 000 000.
+
+Kirjoita SQL-kysely, joka hakee nimet, väkiluvun ja pinta-alan kaikista suurista maista.
+Tulosta taulukko sarakkeilla name, population, area, joka sisältää tiedot vain suurista maista.
+Rivien järjestyksellä ei ole väliä.
 */
 
 SELECT name, population, area
