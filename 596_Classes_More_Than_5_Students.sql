475d9e1843870858ddcc6bde54d172ef998757de
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-14 05:36:44 +0200

    Added a finnish translation to task 596

--- 596_Classes_More_Than_5_Students.sql
+++ 596_Classes_More_Than_5_Students.sql
@@ -1,23 +1,27 @@
 /*
-You are given a table:
-+ Courses:
-    + student: Name of the student;
-    + class: Name of the class;
-    + (student, class) is the primary key.
+You are given a table Courses:
+    - student
+    - class
 
 Each row shows which student is enrolled in which class.
 Write an SQL query to find all classes with at least five students.
 Return the result in any order.
 
-Дана таблица:
-+ Courses:
-    + student: имя студента;
-    + class: название класса;
-    + (student, class) — первичный ключ.
+Дана таблица Courses:
+    - student
+    - class
 
 Каждая строка показывает, в каком классе учится студент.
 Напиши SQL-запрос, который вернёт все классы, в которых минимум 5 студентов.
 Порядок строк в результате любой.
+
+Annettu taulukko Courses:
+    - student
+    - class
+
+Jokainen rivi näyttää, missä luokassa opiskelija opiskelee.
+Kirjoita SQL-kysely, joka palauttaa kaikki luokat, joissa on vähintään 5 opiskelijaa.
+Tuloksen rivien järjestyksellä ei ole väliä.
 */
 
 SELECT class
