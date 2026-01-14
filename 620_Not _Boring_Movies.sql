31c08e62a663c43b68ec385e436756fa7c9c27bb
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-14 06:04:49 +0200

    Added a finnish translation to task 620

--- 620_Not _Boring_Movies.sql
+++ 620_Not _Boring_Movies.sql
@@ -1,25 +1,33 @@
 /*
-You are given a table Cinema that contains information about movies. The table has the following columns:
-+ id (int): the unique ID of the movie (primary key);
-+ movie (varchar): the name of the movie;
-+ description (varchar): the description or genre of the movie;
-+ rating (float): the rating of the movie (with two decimal places, ranging from 0 to 10).
+You are given a table Cinema:
+    - id
+    - movie
+    - description
+    - rating
 
-Your task is to return all movies with an odd-numbered id and a description that is not equal to "boring".
+Return all movies with an odd-numbered id and a description that is not equal to "boring".
 The result should be sorted by rating in descending order.
 
-У вас есть таблица Cinema, содержащая информацию о фильмах. Таблица состоит из следующих столбцов:
-+ id (int) — уникальный идентификатор фильма;
-+ movie (varchar) — название фильма;
-+ description (varchar) — описание фильма;
-+ rating (float) — рейтинг фильма (два знака после запятой, от 0 до 10).
+У вас есть таблица Cinema:
+    - id
+    - movie
+    - description
+    - rating
 
-Необходимо выбрать фильмы, у которых нечетный идентификатор (id) и описание (description) не равно "boring".
-Результат отсортируйте по убыванию рейтинга (rating).
+Необходимо выбрать фильмы, у которых нечетный идентификатор и описание не равно "boring".
+Результат отсортируйте по убыванию рейтинга.
+
+Sinulla on taulukko Cinema:
+    - id
+    - movie
+    - description
+    - rating
+
+Valitse elokuvat, joilla on pariton id ja joiden description ei ole "boring".
+Järjestä tulos rating-sarakkeen mukaan laskevaan järjestykseen.
 */
 
 SELECT *
 FROM Cinema
-WHERE id % 2 = 1
-  AND description != 'boring'
+WHERE id % 2 = 1 AND description != 'boring'
 ORDER BY rating DESC;
