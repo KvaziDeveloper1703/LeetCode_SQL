a85318f4cc2e4c78cee8b7033609bf8d0555fd70
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-15 13:55:29 +0200

    Added a finnish translation to task 1045

--- 1045_Customers_Who_Bought_All_Products.sql
+++ 1045_Customers_Who_Bought_All_Products.sql
@@ -1,29 +1,36 @@
 /*
 You are given two tables:
-+ Customer:
-    + customer_id: ID of the customer (not null);
-    + product_key: ID of the product the customer bought;
-    + The table may contain duplicate rows;
-    + product_key is a foreign key to the Product table,
+Customer:
+    - customer_id
+    - product_key
 
-+ Product:
-    + product_key: Unique ID of the product (primary key).
+Product:
+    - product_key
 
 Write an SQL query to find all customer IDs who bought every product listed in the Product table.
 Return the result in any order.
 
 Даны две таблицы:
-+ Customer:
-    + customer_id: идентификатор покупателя (не NULL);
-    + product_key: идентификатор купленного товара;
-    + В таблице могут быть дубликаты;
-    + product_key — внешний ключ к таблице Product.
+Customer:
+    - customer_id
+    - product_key
 
-+ Product:
-    product_key: уникальный идентификатор товара (первичный ключ);
+Product:
+    product_key
 
 Напиши SQL-запрос, который вернёт ID покупателей, купивших все товары, указанные в таблице Product.
 Порядок строк в результате не важен.
+
+Annetaan kaksi taulua:
+Customer:
+    - customer_id
+    - product_key
+
+Product:
+    - product_key
+
+Kirjoita SQL-kysely, joka palauttaa niiden asiakkaiden ID:t, jotka ovat ostaneet kaikki taulussa Product luetellut tuotteet.
+Rivien järjestyksellä tuloksessa ei ole väliä.
 */
 
 SELECT customer_id
