f0721d3dc5641f97cab2d2dd2d4eac1d9104d3a4
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-15 15:30:36 +0200

    Added a finnish translation to task 1251

--- 1251_Average_Selling_Price.sql
+++ 1251_Average_Selling_Price.sql
@@ -1,40 +1,49 @@
 /*
 You are given two tables:
-+ Prices:
-    + product_id: ID of the product;
-    + start_date, end_date: Period when the price was active;
-    + price: Price during this period;
-    + No overlapping periods for the same product.
+Prices:
+    - product_id
+    - start_date
+    - end_date
+    - price
 
-+ UnitsSold:
-    + product_id: ID of the product;
-    + purchase_date: Date of sale;
-    + units: Number of units sold.
+UnitsSold:
+    - product_id
+    - purchase_date
+    - units
 
 Find the average selling price for each product, rounded to 2 decimal places. If a product was never sold, its average price is 0.
 
 Даны две таблицы:
-+ Prices:
-    + product_id: идентификатор товара;
-    + start_date, end_date: период действия цены;
-    + price: цена в этом периоде;
-    + Для одного товара периоды не пересекаются.
+Prices:
+    - product_id
+    - start_date
+    - end_date
+    - price
 
-+ UnitsSold:
-    + product_id: идентификатор товара;
-    + purchase_date: дата покупки;
-    + units: количество проданных единиц.
+UnitsSold:
+    - product_id
+    - purchase_date
+    - units
 
-Найди среднюю цену продажи для каждого товара, округлённую до двух знаков после запятой. Если товар не продавался — цена считается равной 0.
+Найди среднюю цену продажи для каждого товара, округлённую до двух знаков после запятой. Если товар не продавался - цена считается равной 0.
+
+Annetaan kaksi taulua:
+Prices:
+    - product_id
+    - start_date
+    - end_date
+    - price
+
+UnitsSold:
+    - product_id
+    - purchase_date
+    - units
+
+Etsi jokaiselle tuotteelle keskimääräinen myyntihinta, pyöristettynä kahden desimaalin tarkkuuteen.
+Jos tuotetta ei ole myyty, hinnaksi katsotaan 0.
 */
 
-SELECT  Prices.product_id,
-        ROUND(
-            COALESCE(SUM(UnitsSold.units * Prices.price), 0.0) /
-            COALESCE(SUM(UnitsSold.units), 1),
-            2
-        ) AS average_price
+SELECT Prices.product_id, ROUND(COALESCE(SUM(UnitsSold.units * Prices.price), 0.0) / COALESCE(SUM(UnitsSold.units), 1), 2) AS average_price
 FROM Prices
-LEFT JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id
-                    AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+LEFT JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
 GROUP BY Prices.product_id;
