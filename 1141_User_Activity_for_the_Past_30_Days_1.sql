689a6d3408adcc150a835ca7df0f481044a9a5bc
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-15 14:44:13 +0200

    Added a finnish translation to task 1141

--- 1141_User_Activity_for_the_Past_30_Days_1.sql
+++ 1141_User_Activity_for_the_Past_30_Days_1.sql
@@ -1,31 +1,42 @@
 /*
-You are given a table:
-+ Activity:
-    + user_id: ID of the user;
-    + session_id: ID of the session;
-    + ctivity_date: Date of the activity;
-    + activity_type: One of 'open_session', 'end_session', 'scroll_down', 'send_message'.
+You are given a table Activity:
+    - user_id
+    - session_id
+    - activity_date
+    - activity_type
 
 Each session belongs to exactly one user. The table may have duplicates.
 
-Write an SQL query to find the number of active users per day for the 30-day period ending on 2019-07-27 (inclusive).
+Write an SQL query to find the number of active users per day for the 30-day period ending on 2019-07-27.
 A user is considered active on a given day if they performed at least one activity on that day.
 
 Return the result in any order. Only include days with at least one active user.
 
-Дана таблица:
-+ Activity:
-    + user_id: идентификатор пользователя;
-    + session_id: идентификатор сессии;
-    + activity_date: дата активности;
-    + activity_type: одно из 'open_session', 'end_session', 'scroll_down', 'send_message'.
+Дана таблица Activity:
+    - user_id
+    - session_id
+    - activity_date
+    - activity_type
 
 Каждая сессия принадлежит одному пользователю. В таблице могут быть дубликаты.
 
-Напиши SQL-запрос, который вернёт количество активных пользователей в день за 30-дневный период, заканчивающийся 27 июля 2019 года (включительно).
+Напиши SQL-запрос, который вернёт количество активных пользователей в день за 30-дневный период, заканчивающийся 27 июля 2019 года.
 Пользователь считается активным в день, если у него была хотя бы одна активность в этот день.
 
 Верни результат в любом порядке. Включай только дни, когда были активные пользователи.
+
+Annetaan taulu Activity:
+    - user_id
+    - session_id
+    - activity_date
+    - activity_type
+
+Jokainen istunto (session) kuuluu yhdelle käyttäjälle. Taulussa voi esiintyä duplikaatteja.
+
+Kirjoita SQL-kysely, joka palauttaa aktiivisten käyttäjien määrän päivää kohti 30 päivän ajanjaksolta, joka päättyy 27. heinäkuuta 2019.
+Käyttäjä katsotaan aktiiviseksi tiettynä päivänä, jos hänellä oli vähintään yksi aktiviteetti kyseisenä päivänä.
+
+Palauta tulos missä tahansa järjestyksessä. Ota mukaan vain ne päivät, jolloin aktiivisia käyttäjiä oli.
 */
 
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
