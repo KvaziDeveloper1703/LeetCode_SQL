be563822e7c1ffde8c3b84d0a7f5faf26149e8f5
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-16 10:19:43 +0200

    Added a finnish translation to task 1378

--- 1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
+++ 1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -1,15 +1,17 @@
 /*
 We have two tables:
+Employees:
+    - id
+    - name
 
-+ Table "Employees":
-    + id (integer, unique employee identifier, primary key);
-    + name (string, employee name).
 This table contains a list of all employees in the company.
 
-+ Table "EmployeeUNI":
-    + id (integer, employee ID);
-    + unique_id (integer, employee's unique ID).
+EmployeeUNI:
+    - id
+    - unique_id
+
 The primary key for this table is the combination of (id, unique_id).
+
 This table maps regular employee ids to their unique_id.
 However, not all employees have a unique_id, so some ids from Employees may be missing in EmployeeUNI.
 
@@ -17,22 +19,24 @@ Write an SQL query to join the data from both tables and return the unique_id of
 If an employee does not have a unique_id, the column should contain NULL.
 
 Return a table with the following columns:
-+ unique_id (employee's unique identifier or NULL if no unique_id exists);
-+ name (employee's name).
+    - unique_id
+    - name
 
 The result order can be arbitrary.
 
 У нас есть две таблицы:
+Employees:
+    - id
+    - name
 
-+ Таблица "Employees":
-    + id (целое число, уникальный идентификатор сотрудника, первичный ключ);
-    + name (строка, имя сотрудника).
 Эта таблица содержит список всех сотрудников компании.
 
-+ Таблица "EmployeeUNI":
-    + id (целое число, идентификатор сотрудника);
-    + unique_id (целое число, уникальный идентификатор сотрудника).
+EmployeeUNI:
+    - id
+    - unique_id
+
 Первичным ключом этой таблицы является комбинация (id, unique_id).
+
 Эта таблица сопоставляет обычные идентификаторы сотрудников с их уникальными идентификаторами.
 Однако не у всех сотрудников есть уникальный идентификатор, поэтому некоторые id из таблицы Employees могут отсутствовать в таблице EmployeeUNI.
 
@@ -40,13 +44,12 @@ The result order can be arbitrary.
 Если у сотрудника нет unique_id, в соответствующей колонке должно быть значение NULL.
 
 Верните таблицу со следующими колонками:
-+ unique_id (уникальный идентификатор сотрудника или NULL, если его нет);
-+ name (имя сотрудника).
+    - unique_id
+    - name
 
 Порядок строк в результате может быть произвольным.
 */
 
 SELECT EmployeeUNI.unique_id, Employees.name
 FROM Employees
-LEFT JOIN EmployeeUNI
-ON Employees.id = EmployeeUNI.id;
+LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
