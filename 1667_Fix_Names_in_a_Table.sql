383b7f29a01906cb80e9e16856fdd481865364dd
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-16 10:53:50 +0200

    Added a finnish translation to task 1667

--- 1667_Fix_Names_in_a_Table.sql
+++ 1667_Fix_Names_in_a_Table.sql
@@ -1,27 +1,36 @@
 /*
-You are given a table:
-+ Users:
-    + user_id: unique identifier of the user (primary key);
-    + name: name of the user, containing only uppercase and lowercase letters.
+You are given a table Users:
+    - user_id
+    - name
 
 Write an SQL query to format the names so that only the first character is uppercase, and the rest are lowercase.
 Return the result table ordered by user_id.
 
 Return the following columns:
-+ user_id;
-+ name — with correct capitalization (e.g., aLice → Alice, bOB → Bob).
+    - user_id
+    - name
 
-Дана таблица:
-+ Users:
-    + user_id: уникальный идентификатор пользователя (первичный ключ);
-    + name: имя пользователя, содержащее только латинские буквы в верхнем и нижнем регистрах.
+Дана таблица Users:
+    - user_id
+    - name
 
-Напиши SQL-запрос, который исправит имена так, чтобы только первая буква была заглавной, а остальные — строчными.
+Напиши SQL-запрос, который исправит имена так, чтобы только первая буква была заглавной, а остальные - строчными.
 Результат должен быть отсортирован по user_id.
 
 Верни таблицу со следующими колонками:
-+ user_id;
-+ name — имя с правильным форматированием (например, aLice → Alice, bOB → Bob).
+    - user_id
+    - name
+
+Annetaan taulu Users:
+    - user_id
+    - name
+
+Kirjoita SQL-kysely, joka korjaa nimet niin, että vain ensimmäinen kirjain on iso ja loput ovat pieniä kirjaimia.
+Tuloksen tulee olla järjestetty sarakkeen user_id mukaan.
+
+Palauta taulu, jossa on seuraavat sarakkeet:
+    - user_id
+    - name
 */
 
 SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
