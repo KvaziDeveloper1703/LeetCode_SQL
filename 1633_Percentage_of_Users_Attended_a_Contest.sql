a1f362910e7d113d1e21d61173a02ca58f810c2c
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-16 10:47:30 +0200

    Added a finnish translation to task 1633

--- 1633_Percentage_of_Users_Attended_a_Contest.sql
+++ 1633_Percentage_of_Users_Attended_a_Contest.sql
@@ -1,33 +1,42 @@
 /*
 You are given two tables:
-+ Users:
-    + user_id: ID of the user (primary key);
-    + user_name: Name of the user.
+Users:
+    - user_id
+    - user_name
 
-+ Register:
-    + contest_id: ID of the contest;
-    + user_id: ID of the user who registered for the contest;
-    + (contest_id, user_id) is the primary key.
+Register:
+    - contest_id
+    - user_id
 
 Write an SQL query to find the percentage of users who registered for each contest, rounded to two decimal places.
 Return the result sorted by percentage in descending order, and by contest_id in ascending order if there is a tie.
 
 Даны две таблицы:
-+ Users:
-    + user_id: идентификатор пользователя (первичный ключ);
-    + user_name: имя пользователя.
+Users:
+    - user_id
+    - user_name
 
-+ Register
-    + contest_id: идентификатор контеста;
-    + user_id: идентификатор пользователя, зарегистрированного на контест;
-    + (contest_id, user_id) — первичный ключ.
+Register
+    - contest_id
+    - user_id
 
 Напиши SQL-запрос, который вернёт процент пользователей, зарегистрированных на каждый контест, округлённый до двух знаков после запятой.
 Результат отсортируй по убыванию percentage, а при равенстве — по возрастанию contest_id.
+
+Annetaan kaksi taulua:
+Users:
+    - user_id
+    - user_name
+
+Register:
+    - contest_id
+    - user_id
+
+Kirjoita SQL-kysely, joka palauttaa niiden käyttäjien prosenttiosuuden, jotka ovat rekisteröityneet kuhunkin kilpailuun, pyöristettynä kahteen desimaaliin.
+Järjestä tulos laskevasti sarakkeen percentage mukaan, ja tasatilanteessa nousevasti sarakkeen contest_id mukaan.
 */
 
-SELECT  Register.contest_id,
-        ROUND(COUNT(DISTINCT Register.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+SELECT Register.contest_id, ROUND(COUNT(DISTINCT Register.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
 FROM Register
 GROUP BY Register.contest_id
 ORDER BY percentage DESC, Register.contest_id ASC;
