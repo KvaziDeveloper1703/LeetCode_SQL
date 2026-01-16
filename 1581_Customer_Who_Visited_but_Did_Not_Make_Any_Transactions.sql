781cb2090d1a779f75d624a31c73ffd77403aa72
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-16 10:45:07 +0200

    Added a finnish translation to task 1581

--- 1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
+++ 1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -1,45 +1,69 @@
 /*
 We have two tables:
+Visits:
+    - visit_id
+    - customer_id
 
-+ Table "Visits":
-    + visit_id (integer, unique visit identifier);
-    + customer_id (integer, customer identifier).
 This table contains information about customers who visited the mall.
 
-+ Table "Transactions":
-    + transaction_id (integer, unique transaction identifier);
-    + visit_id (integer, visit identifier, referencing Visits);
-    + amount (integer, transaction amount).
+Transactions:
+    - transaction_id
+    - visit_id
+    - amount
+
 This table contains information about purchases made during each visit_id.
 
 Write an SQL query to find the IDs of customers who visited the mall without making any transactions and count how many times they made such visits.
 
 Return a table with the following columns:
-+ customer_id (customer ID);
-+ count_no_trans (number of visits without transactions).
+    - customer_id
+    - count_no_trans
 
 The result should be sorted in any order.
 
 У нас есть две таблицы:
+Visits:
+    - visit_id
+    - customer_id
 
-+ Таблица "Visits":
-    + visit_id (целое число, уникальный идентификатор визита);
-    + customer_id (целое число, идентификатор клиента).
 Эта таблица содержит информацию о клиентах, которые посещали торговый центр.
 
-+ Таблица "Transactions":
-    + transaction_id (целое число, уникальный идентификатор транзакции);
-    + visit_id (целое число, идентификатор визита, связанный с Visits);
-    + amount (целое число, сумма транзакции).
+Transactions:
+    - transaction_id
+    - visit_id
+    - amount
+
 Эта таблица содержит информацию о покупках, совершённых во время каждого визита (visit_id).
 
 Написать SQL-запрос, который находит ID клиентов, посещавших торговый центр без совершения покупок, а также количество таких визитов для каждого клиента.
 
 Вывести таблицу с колонками:
-+ customer_id (ID клиента);
-+ count_no_trans (количество визитов без транзакций).
+    - customer_id
+    - count_no_trans
 
 Результат должен быть отсортирован в любом порядке.
+
+Meillä on kaksi taulua:
+Visits:
+    - visit_id
+    - customer_id
+
+Tämä taulu sisältää tiedot asiakkaista, jotka ovat käyneet ostoskeskuksessa.
+
+Transactions:
+    - transaction_id
+    - visit_id
+    - amount
+
+Tämä taulu sisältää tiedot ostoksista, jotka on tehty jokaisen käynnin (visit_id) aikana.
+
+Kirjoita SQL-kysely, joka löytää niiden asiakkaiden tunnisteet, jotka kävivät ostoskeskuksessa tekemättä ostoksia, sekä tällaisten käyntien määrän jokaiselle asiakkaalle.
+
+Tulosta taulu sarakkeilla:
+    - customer_id
+    - count_no_trans
+
+Tuloksen järjestys voi olla mikä tahansa.
 */
 
 SELECT Visits.customer_id, COUNT(DISTINCT Visits.visit_id) AS count_no_trans
