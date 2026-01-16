6bdfa5ba1e477d160c00333552d3396c84ddde43
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-16 10:42:12 +0200

    Added a finnish translation to task 1527

--- 1527_Patients_With_a_Condition.sql
+++ 1527_Patients_With_a_Condition.sql
@@ -1,35 +1,51 @@
 /*
-You are given a table:
-+ Patients:
-    + patient_id: unique ID of the patient (primary key);
-    + patient_name: name of the patient;
-    + conditions: a space-separated string of condition codes (can be empty).
+You are given a table Patients:
+    - patient_id
+    - patient_name
+    - conditions
 
 Each row contains information about one patient in the hospital.
 
 Write an SQL query to find the patients who have Type I Diabetes, which is identified by any condition code that starts with the prefix DIAB1.
+
 Return the result with the following columns:
-+ patient_id;
-+ patient_name;
-+ conditions.
+    - patient_id
+    - patient_name
+    - conditions
 
 The result can be returned in any order.
 
-Дана таблица:
-+ Patients:
-    + patient_id: уникальный идентификатор пациента (первичный ключ);
-    + patient_name: имя пациента;
-    + conditions: строка, содержащая коды заболеваний, разделённые пробелами (может быть пустой).
+Дана таблица Patients:
+    - patient_id
+    - patient_name
+    - conditions
 
 Каждая строка содержит информацию об одном пациенте в больнице.
 
 Напиши SQL-запрос, который найдёт всех пациентов с сахарным диабетом 1 типа — его обозначает любой код, начинающийся с префикса DIAB1.
+
 Верни результат с колонками:
-+ patient_id;
-+ patient_name;
-+ conditions.
+    - patient_id
+    - patient_name
+    - conditions
 
 Порядок строк в результате может быть произвольным.
+
+Annetaan taulu Patients:
+    - patient_id
+    - patient_name
+    - conditions
+
+Jokainen rivi sisältää tiedot yhdestä sairaalan potilaasta.
+
+Kirjoita SQL-kysely, joka löytää kaikki potilaat, joilla on tyypin 1 diabetes. Se tunnistetaan kaikista koodeista, jotka alkavat etuliitteellä DIAB1.
+
+Palauta tulos sarakkeilla:
+    - patient_id
+    - patient_name
+    - conditions
+
+Rivien järjestys tuloksessa voi olla mikä tahansa.
 */
 
 SELECT patient_id, patient_name, conditions
