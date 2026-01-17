9bf3ed7e5d16be3cc026124596d5b508cd584838
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-17 09:26:54 +0200

    Added a finnish translation to task 1731

--- 1731_The_Number_of_Employees_Which_Report_to_Each_Employee.sql
+++ 1731_The_Number_of_Employees_Which_Report_to_Each_Employee.sql
@@ -1,41 +1,36 @@
 /*
-You are given a table:
-+ Employees:
-    + employee_id: Unique ID of the employee;
-    + name: Name of the employee;
-    + reports_to: ID of the manager this employee reports to (can be null);
-    + age: Age of the employee.
+You are given a table Employees:
+    - employee_id
+    - name
+    - reports_to
+    - age
 
 A manager is an employee who has at least one other employee reporting directly to them.
 
 Write an SQL query to return:
-+ employee_id and name of each manager;
-+ reports_count: number of employees who report directly to them;
-+ average_age: average age of their direct reports, rounded to the nearest integer.
+    - employee_id
+    - reports_count
+    - average_age
 
 Return the result sorted by employee_id.
 
-Дана таблица:
-+ Employees:
-    + employee_id: уникальный идентификатор сотрудника;
-    + name: имя сотрудника;
-    + reports_to: ID руководителя, которому он подчиняется (может быть null);
-    + age: возраст сотрудника.
+Дана таблица Employees:
+    - employee_id
+    - name
+    - reports_to
+    - age
 
-Руководитель — это сотрудник, у которого есть хотя бы один прямой подчинённый.
+Руководитель - это сотрудник, у которого есть хотя бы один прямой подчинённый.
 
 Напиши SQL-запрос, который вернёт:
-+ employee_id и name каждого руководителя;
-+ reports_count: количество сотрудников, которые напрямую ему подчиняются;
-+ average_age: средний возраст этих подчинённых, округлённый до ближайшего целого.
+    - employee_id
+    - reports_count
+    - average_age
 
 Результат отсортируй по employee_id.
 */
 
-SELECT  Employees.employee_id,
-        Employees.name,
-        COUNT(Reports.employee_id) AS reports_count,
-        ROUND(AVG(Reports.age)) AS average_age
+SELECT Employees.employee_id, Employees.name, COUNT(Reports.employee_id) AS reports_count, ROUND(AVG(Reports.age)) AS average_age
 FROM Employees
 JOIN Employees AS Reports ON Employees.employee_id = Reports.reports_to
 GROUP BY Employees.employee_id, Employees.name
