726fc855b575a4c9a11c0bd218b0411cd2b9d9db
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-17 09:23:52 +0200

    Added a finnish translation to task 1729

--- 1729_Find_Followers_Count.sql
+++ 1729_Find_Followers_Count.sql
@@ -1,21 +1,27 @@
 /*
-You are given a table:
-+ Followers:
-    + user_id: The ID of the user being followed;
-    + follower_id: The ID of the follower;
-    + (user_id, follower_id) is the primary key.
+You are given a table Followers:
+    - user_id
+    - follower_id
 
 Each row indicates that follower_id follows user_id.
+
 Write an SQL query to return the number of followers for each user, sorted by user_id in ascending order.
 
-Дана таблица:
-+ Followers:
-    + user_id: идентификатор пользователя, на которого подписались;
-    + follower_id: идентификатор подписчика;
-    + (user_id, follower_id) — первичный ключ.
+Дана таблица Followers:
+    - user_id
+    - follower_id
 
 Каждая строка показывает, что follower_id подписан на user_id.
+
 Напиши SQL-запрос, который вернёт количество подписчиков для каждого пользователя, отсортированных по user_id по возрастанию.
+
+Annetaan taulu Followers:
+    - user_id
+    - follower_id
+
+Jokainen rivi tarkoittaa, että follower_id seuraa käyttäjää user_id.
+
+Kirjoita SQL-kysely, joka palauttaa seuraajien lukumäärän jokaiselle käyttäjälle, järjestettynä user_id-sarakkeen mukaan nousevaan järjestykseen.
 */
 
 SELECT user_id, COUNT(follower_id) AS followers_count
