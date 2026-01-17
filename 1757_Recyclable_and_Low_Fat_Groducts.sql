1fc305e3f8df6e81e83ba881b249ab789962876e
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-17 09:31:28 +0200

    Added a finnish translation to task 1757

--- 1757_Recyclable_and_Low_Fat_Groducts.sql
+++ 1757_Recyclable_and_Low_Fat_Groducts.sql
@@ -1,21 +1,27 @@
 /*
-We have a table "Products" with the following columns:
-+ product_id (integer, primary key);
-+ low_fats (ENUM type 'Y', 'N'), where 'Y' means the product is low fat, and 'N' means it is not;
-+ recyclable (ENUM type 'Y', 'N'), where 'Y' means the product is recyclable, and 'N' means it is not.
+We have a table Products:
+    - product_id
+    - low_fats
+    - recyclable
 
 You need to write an SQL query to find the product IDs of the products that are both low fat (low_fats = 'Y') and recyclable (recyclable = 'Y').
-
 Return a table with a single column product_id containing the IDs of products that meet the conditions. The result order can be arbitrary.
 
 У нас есть таблица "Products" со следующими колонками:
-+ product_id (целое число, первичный ключ);
-+ low_fats (перечислимый тип ENUM('Y', 'N')), где 'Y' означает, что продукт является низкожировым, а 'N' — что не является;
-+ recyclable (перечислимый тип ENUM('Y', 'N')), где 'Y' означает, что продукт подлежит переработке, а 'N' — что не подлежит.
+    - product_id
+    - low_fats
+    - recyclable
 
 Требуется написать SQL-запрос, который находит id продуктов, которые одновременно являются низкожировыми (low_fats = 'Y') и подлежат переработке (recyclable = 'Y').
-
 Вывести таблицу с одним столбцом product_id, содержащую идентификаторы продуктов, удовлетворяющих этим условиям. Порядок строк в ответе может быть произвольным.
+
+Meillä on taulu Products, jossa on seuraavat sarakkeet:
+    - product_id
+    - low_fats
+    - recyclable
+
+Tehtävänä on kirjoittaa SQL-kysely, joka hakee niiden tuotteiden tunnisteet, jotka ovat sekä vähärasvaisia (low_fats = 'Y') että kierrätettäviä (recyclable = 'Y').
+Tulosta taulu, jossa on yksi sarake product_id, ja jossa on näiden ehdot täyttävien tuotteiden tunnisteet. Rivien järjestyksellä ei ole väliä.
 */
 
 SELECT product_id
