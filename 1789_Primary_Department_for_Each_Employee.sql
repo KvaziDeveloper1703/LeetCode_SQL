e73ef8dd60a450d48f6c15fd3de286078ec7f3f7
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-17 09:45:18 +0200

    Added a finnish translation to task 1789

--- 1789_Primary_Department_for_Each_Employee.sql
+++ 1789_Primary_Department_for_Each_Employee.sql
@@ -1,10 +1,8 @@
 /*
-You are given a table:
-+ Employee:
-    + employee_id: ID of the employee;
-    + department_id: ID of the department the employee is part of;
-    + primary_flag: 'Y' if this is the employee's primary department, 'N' otherwise;
-    + (employee_id, department_id) is the primary key.
+You are given a table Employee:
+    - employee_id
+    - department_id
+    - primary_flag
 
 Each employee can belong to multiple departments, but must have one primary department.
 If an employee belongs to only one department, their primary_flag will be 'N', but that department should still be treated as their primary.
@@ -14,20 +12,31 @@ For employees with only one department, return that department.
 
 Return results in any order.
 
-Дана таблица:
-+ Employee:
-    + employee_id: ID сотрудника;
-    + department_id: ID отдела, к которому он принадлежит;
-    + primary_flag: 'Y' — если это основной отдел, 'N' — если нет;
-    + (employee_id, department_id) — составной первичный ключ.
+Дана таблица Employee:
+    - employee_id
+    - department_id
+    - primary_flag
 
 Сотрудник может состоять в нескольких отделах, но должен иметь один основной отдел.
 Если сотрудник состоит только в одном отделе, то primary_flag будет 'N', но этот отдел всё равно считается основным.
 
 Напиши SQL-запрос, который вернёт ID сотрудников и ID их основного отдела.
-Для сотрудников с одним отделом — возвращай этот единственный отдел.
+Для сотрудников с одним отделом - возвращай этот единственный отдел.
 
 Порядок строк в результате не имеет значения.
+
+Annetaan taulu Employee:
+    - employee_id
+    - department_id
+    - primary_flag
+
+Työntekijä voi kuulua useaan osastoon, mutta hänellä täytyy olla yksi pääosasto.
+Jos työntekijä kuuluu vain yhteen osastoon, silloin primary_flag on 'N', mutta kyseinen osasto katsotaan silti pääosastoksi.
+
+Kirjoita SQL-kysely, joka palauttaa työntekijöiden tunnisteet ja heidän pääosastonsa tunnisteen.
+Niille työntekijöille, joilla on vain yksi osasto, palauta tämä ainoa osasto.
+
+Rivien järjestyksellä ei ole väliä.
 */
 
 SELECT employee_id, department_id
