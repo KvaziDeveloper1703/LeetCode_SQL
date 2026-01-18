c66cf9bebb022c9a81198a40f03d77e67a607614
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-18 12:58:51 +0200

    Added a finnish translation to task 3570

--- 3570_find_books_with_no_available_copies.sql
+++ 3570_find_books_with_no_available_copies.sql
@@ -1,67 +1,101 @@
 /*
 You are given two tables:
 Library_books:
-    - book_id;
-    - title;
-    - author;
-    - genre;
-    - publication_year;
-    - total_copies - total number of copies owned by the library.
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - total_copies
 
 Borrowing_records:
-    - record_id;
-    - book_id;
-    - borrower_name;
-    - borrow_date;
-    - return_date - NULL if the book has not been returned yet.
+    - record_id
+    - book_id
+    - borrower_name
+    - borrow_date
+    - return_date
 
 A book is considered currently borrowed if there exists at least one borrowing record with return_date IS NULL.
 
 Write an SQL query to find all books that: are currently borrowed, and have zero available copies.
 
 For each such book, return:
-    - book_id;
-    - title;
-    - author;
-    - genre;
-    - publication_year;
-    - current_borrowers.
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - current_borrowers
 
 Order the result by:
-    - current_borrowers in descending order
+    - current_borrowers in descending order;
     - title in ascending order.
 
 Даны две таблицы:
 Library_books:
-    - book_id;
-    - title;
-    - author;
-    - genre;
-    - publication_year;
-    - total_copies - общее количество экземпляров книги в библиотеке.
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - total_copies
 
 Borrowing_records:
-    - record_id;
-    - book_id;
-    - borrower_name;
-    - borrow_date;
-    - return_date - NULL, если книга ещё не возвращена.
+    - record_id
+    - book_id
+    - borrower_name
+    - borrow_date
+    - return_date
 
 Книга считается в данный момент выданной, если существует хотя бы одна запись с return_date IS NULL.
 
 Напишите SQL-запрос, который находит все книги, которые: в данный момент выданы, и имеют ноль доступных экземпляров.
 
 Для каждой такой книги выведите:
-    - book_id;
-    - title;
-    - author;
-    - genre;
-    - publication_year;
-    - current_borrowers - количество записей с return_date IS NULL.
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - current_borrowers
 
 Отсортируйте результат:
-    - по current_borrowers по убыванию
+    - по current_borrowers по убыванию;
     - по title по возрастанию.
+
+Annetaan kaksi taulua:
+Library_books:
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - total_copies
+
+Borrowing_records:
+    - record_id
+    - book_id
+    - borrower_name
+    - borrow_date
+    - return_date
+
+Kirjan katsotaan olevan tällä hetkellä lainassa, jos löytyy vähintään yksi rivi, jossa return_date IS NULL.
+
+Kirjoita SQL-kysely, joka löytää kaikki kirjat, jotka:
+    - ovat tällä hetkellä lainassa, ja
+    - joista ei ole yhtään saatavilla olevaa kappaletta.
+
+Jokaiselle tällaiselle kirjalle tulosta:
+    - book_id
+    - title
+    - author
+    - genre
+    - publication_year
+    - current_borrowers
+
+Järjestä tulos:
+    - current_borrowers laskevaan järjestykseen;
+    - title nousevaan järjestykseen.
 */
 
 SELECT library_books.book_id, library_books.title, library_books.author, library_books.genre, library_books.publication_year, COUNT(borrowing_records.record_id) AS current_borrowers
