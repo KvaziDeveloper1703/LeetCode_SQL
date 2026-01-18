48c23304e74e2ea84f5cdb30d4129dcb4f793813
Viktor <viktor.sbruev.1999@gmail.com>
2026-01-18 12:16:52 +0200

    Added a finnish translation to task 2356

--- 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql
+++ 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql
@@ -1,23 +1,21 @@
 /*
-You are given a table:
-+ Teacher:
-    + teacher_id: ID of the teacher;
-    + subject_id: ID of the subject;
-    + dept_id: ID of the department;
-    + (subject_id, dept_id) is the primary key.
+You are given a table Teacher:
+    - teacher_id
+    - subject_id
+    - dept_id
 
 Each row shows that a teacher teaches a subject in a specific department.
+
 Write an SQL query to return, for each teacher, the number of unique subjects they teach, regardless of department.
 
-Дана таблица:
-+ Teacher:
-    + teacher_id: идентификатор преподавателя;
-    + subject_id: идентификатор предмета;
-    + dept_id: идентификатор кафедры;
-    + (subject_id, dept_id) — первичный ключ.
+Дана таблица Teacher:
+    - teacher_id
+    - subject_id
+    - dept_id
 
 Каждая строка показывает, что преподаватель ведёт предмет в определённой кафедре.
-Напиши SQL-запрос, который вернёт для каждого преподавателя количество уникальных предметов, которые он преподаёт (независимо от кафедры).
+
+Напиши SQL-запрос, который вернёт для каждого преподавателя количество уникальных предметов, которые он преподаёт.
 */
 
 SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
